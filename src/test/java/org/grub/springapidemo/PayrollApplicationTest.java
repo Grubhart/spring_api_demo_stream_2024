4180f8896d70bfaa96035839c0bcf2d6902a3977
grubhart <grubhart@gmail.com>
2024-07-17 00:52:00 -0600

    added integration with TestContainer to Context Load Test

--- src/test/java/org/grub/springapidemo/PayrollApplicationTest.java
+++ src/test/java/org/grub/springapidemo/PayrollApplicationTest.java
@@ -2,9 +2,15 @@ package org.grub.springapidemo;
 
 import org.grub.springapidemo.controller.EmployeeController;
 import org.grub.springapidemo.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,6 +20,29 @@ class PayrollApplicationTest {
 
     @Autowired
     private EmployeeController controller;
+
+    static DockerImageName mysqlImage = DockerImageName.parse("container-registry.oracle.com/mysql/community-server:latest").asCompatibleSubstituteFor("mysql");
+
+
+    static MySQLContainer<?> mySQL = new MySQLContainer<>(mysqlImage);
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        mySQL.start();
+
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQL::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQL::getUsername);
+        registry.add("spring.datasource.password", mySQL::getPassword);
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        mySQL.stop();
+    }
 
     @Test
     void contextLoads() {
